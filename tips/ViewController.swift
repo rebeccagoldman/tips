e96314e086d66858ea9c251597672c08446499ed
Your Name <rebecca@anyroad.com>
2015-04-02 00:18:54 -0700

    Show/hide tip controller. Constant $ symbol.

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var tipButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,26 +28,66 @@ class ViewController: UIViewController {
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
         
+        tipControl.alpha = 0
+    }
+
+    
+    @IBAction func tipButtonToggle(sender: UIButton) {
+        
+        if(tipControl.alpha == 1) {
+            tipControl.alpha = 0
+            println("Tip Control is hidden")
+            
+        } else {
+            
+            tipControl.alpha = 1
+            println("Tip Control is visible")
+
+            
+            
+        }
     }
 
     
     
     @IBAction func onEditingChanged(sender: AnyObject) {
         
+        var billString = billField.text
         
-        var tipPercentages = [0.18, 0.2, 0.22]
+        if(countElements(billString) > 0) {
+            
+            if(billString.substringToIndex(advance(billString.startIndex, 1)) == "$") {
+                billString = billString.substringFromIndex(advance(billString.startIndex,1))
+            }
+        }
+        
+        var tipPercentages = [0.15, 0.2, 0.25]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
-        var billAmount = NSString(string: billField.text).doubleValue
+        var billAmount = NSString(string: billString).doubleValue
+        
+      
+        
+        
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
+        billField.text = "$"+"\(billString)"
+
+        
+        var numberOfPeople = 2
+        
+    
+        
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
-        
+
         
 
     }
+    
+    
+    
     
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
